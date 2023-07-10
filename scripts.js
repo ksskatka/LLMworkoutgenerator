48223328bdbb2d9654db62571908a4417e0194d1
Kaspar Skatka <101766807+ksskatka@users.noreply.github.com>
2023-07-10 06:30:57 +0200

    Update scripts.js
    *IMPORTANT* added the function that takes and (for now) stores and validates input from the form. Currently ouputs to the console, but will be parsed to the API.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,126 @@ function enterClick(){
     document.getElementById('inputmenu').classList.add('fadeinanim');
 }
 
+/*
+    WILL TIDY UP AND SHORTEN THE CODE BELOW, JUST FOR QUICK AND VISIBLE TWEAKING.
+*/
+
+function submitClick(){
+    event.preventDefault();
+    var information = {
+        a : document.getElementsByName("input1")[0].value,
+        b : document.getElementsByName("input2")[0].value,
+        c : document.getElementsByName("input3")[0].value,
+        d : document.getElementsByName("input4")[0].value,
+        e : document.getElementsByName("input5")[0].value,
+    };
+
+    var validcheck = true;
+    if (information.a == "" || information.a == null) {
+        document.getElementById('error1').style.visibility="visible";
+        document.getElementById('error1').style.animation = 'none';
+        document.getElementById('error1').offsetHeight; /* trigger reflow */
+        document.getElementById('error1').style.animation = null;
+        if (document.getElementById('error1').classList.contains('errorfadeinanim')) {
+            document.getElementById('error1').classList.add('popoutanim');
+        }
+        else {
+            document.getElementById('error1').classList.add('errorfadeinanim');
+        }
+        validcheck = false;
+    }
+    else if (information.a != "" && information.a != null) {
+        document.getElementById('error1').style.visibility="hidden";
+        document.getElementById('error1').classList.remove('errorfadeinanim');
+        document.getElementById('error1').classList.remove('popoutanim');
+    }
+
+    if (information.b == "" || information.b == null) {
+        document.getElementById('error2').style.visibility="visible";
+        document.getElementById('error2').style.visibility="visible";
+        document.getElementById('error2').style.animation = 'none';
+        document.getElementById('error2').offsetHeight; /* trigger reflow */
+        document.getElementById('error2').style.animation = null;
+        if (document.getElementById('error2').classList.contains('errorfadeinanim')) {
+            document.getElementById('error2').classList.add('popoutanim');
+        }
+        else {
+            document.getElementById('error2').classList.add('errorfadeinanim');
+        }
+        validcheck = false;
+    }
+    else if (information.b != "" && information.b != null) {
+        document.getElementById('error2').style.visibility="hidden";
+        document.getElementById('error2').classList.remove('errorfadeinanim');
+        document.getElementById('error2').classList.remove('popoutanim');
+    }
+
+    if (information.c == "" || information.c == null) {
+        document.getElementById('error3').style.visibility="visible";
+        document.getElementById('error3').style.visibility="visible";
+        document.getElementById('error3').style.animation = 'none';
+        document.getElementById('error3').offsetHeight; /* trigger reflow */
+        document.getElementById('error3').style.animation = null;
+        if (document.getElementById('error3').classList.contains('errorfadeinanim')) {
+            document.getElementById('error3').classList.add('popoutanim');
+        }
+        else {
+            document.getElementById('error3').classList.add('errorfadeinanim');
+        }
+        validcheck = false;
+    }
+    else if (information.c != "" && information.c != null) {
+        document.getElementById('error3').style.visibility="hidden";
+        document.getElementById('error3').classList.remove('errorfadeinanim');
+        document.getElementById('error3').classList.remove('popoutanim');
+    }
+
+    if (information.d == "" || information.d == null) {
+        document.getElementById('error4').style.visibility="visible";
+        document.getElementById('error4').style.visibility="visible";
+        document.getElementById('error4').style.animation = 'none';
+        document.getElementById('error4').offsetHeight; /* trigger reflow */
+        document.getElementById('error4').style.animation = null;
+        if (document.getElementById('error4').classList.contains('errorfadeinanim')) {
+            document.getElementById('error4').classList.add('popoutanim');
+        }
+        else {
+            document.getElementById('error4').classList.add('errorfadeinanim');
+        }
+        validcheck = false;
+    }
+    else if (information.d != "" && information.d != null) {
+        document.getElementById('error4').style.visibility="hidden";
+        document.getElementById('error4').classList.remove('errorfadeinanim');
+        document.getElementById('error4').classList.remove('popoutanim');
+    }
+
+    if (information.e == "" || information.e == null) {
+        document.getElementById('error5').style.visibility="visible";
+        document.getElementById('error5').style.visibility="visible";
+        document.getElementById('error5').style.animation = 'none';
+        document.getElementById('error5').offsetHeight; /* trigger reflow */
+        document.getElementById('error5').style.animation = null;
+        if (document.getElementById('error5').classList.contains('errorfadeinanim')) {
+            document.getElementById('error5').classList.add('popoutanim');
+        }
+        else {
+            document.getElementById('error5').classList.add('errorfadeinanim');
+        }
+        validcheck = false;
+    }
+    else if (information.e != "" || information.e != null) {
+        document.getElementById('error5').style.visibility="hidden";
+        document.getElementById('error5').classList.remove('errorfadeinanim');
+        document.getElementById('error5').classList.remove('popoutanim');
+    }
+
+    if (validcheck == true) {
+        console.log(information.a);
+        console.log(information.b);
+        console.log(information.c);
+        console.log(information.d);
+        console.log(information.e);
+    }
+}
+
