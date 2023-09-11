fadeaf3194f991ecdccac70f4bbc125c7de8d1c9
ksskatka <kasparslothskatka@gmail.com>
2023-09-10 20:15:24 -0500

    Version 1.0. First anticipated production version.

--- public/scripts.js
+++ public/scripts.js
@@ -5,11 +5,61 @@ enterbtn.addEventListener("click", function() {
     document.getElementById('front').classList.add('fadeoutanim'); 
     document.getElementById('inputmenu').classList.add('fadeinanim');
 })
- 
+
+const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
+
+async function askGPT(param1, param2, param3, param4, param5) {
+    const promptText = `I want you to generate a strength training workout for me today. The number of exercises I want is ${param1}. I want to work on ${param2}. My height is ${param3}. My weight is ${param4}. I am a(n) ${param5} in the gym. I would like to train hard, but please make sure that the exercises are not way too overloaded with their sets and repetitions! Use this format: '{ "exercises": [ {"exercise": "bench press", "sets": 4, "repetitions": 8, "weight": 135}, {"exercise": "squats", "sets": 3, "repetitions": 8, "weight": 210}, {"exercise": "lat pulldowns", "sets": 3, "repetitions": 8, "weight": 85}, {"exercise": "tricep pulldown", "sets": 3, "repetitions": 8, "weight": 60} ] }'. Return ONLY JSON.`;
+
+    //view raw input
+    /*console.log(promptText);*/
+    let parsedResponse = "";
+    
+    try {
+        const response = await fetch('/getGPT3Response', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ prompt: promptText })
+        });
+
+        const data = await response.json();
+        parsedResponse = JSON.parse(data.message);
+        //view raw response
+        /*console.log(parsedResponse);*/
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+    
+    document.getElementById('loader').style.visibility="hidden";
+    document.getElementById('resulttable').classList.add('fadeinanimtable');
+    let table = document.getElementById('resulttable');
+    let exercisesArray = parsedResponse.exercises;
+
+// Access each exercise 
+    var counter = 1;
+    exercisesArray.forEach(exercise => {
+        var row = table.insertRow(counter);
+        if (counter%2 == 1) {
+            row.style.backgroundColor="#ffffff";
+        }
+        else {
+            row.style.backgroundColor="#f3f3f3";
+        }
+        row.style.color="#000000";
+        var cell1 = row.insertCell(0); cell1.innerHTML = exercise.exercise;
+        var cell2 = row.insertCell(1); cell2.innerHTML = exercise.sets;
+        var cell3 = row.insertCell(2); cell3.innerHTML = exercise.repetitions;
+        var cell4 = row.insertCell(3); cell4.innerHTML = exercise.weight;
+        counter++;
+    });
+
+}
+
+//called upon clicking the "generate" button, parses info and formats it into a table
 submitbtn.addEventListener("click", function() {
-    /*
-    WILL TIDY UP AND SHORTEN THE CODE BELOW, JUST FOR QUICK AND VISIBLE TWEAKING.
-    */ 
     let information = {
         a : document.getElementsByName("input1")[0].value,
         b : document.getElementsByName("input2")[0].value,
@@ -18,6 +68,7 @@ submitbtn.addEventListener("click", function() {
         e : document.getElementsByName("input5")[0].value,
     };
     
+    //the below blocks of code check for valid input, if not the input needs to be modified
     let validcheck = true;
     if (information.a == "" || information.a == null) {
         document.getElementById('error1').style.visibility="visible";
@@ -119,15 +170,13 @@ submitbtn.addEventListener("click", function() {
     }
     
     if (validcheck == true) {
-        console.log(information.a);
-        console.log(information.b);
-        console.log(information.c);
-        console.log(information.d);
-        console.log(information.e);
-
         document.getElementById('inputmenu').classList.remove('fadeinanim'); 
-        document.getElementById('inputmenu').style.visibility="visible"; 
+        document.getElementById('inputmenu').style.visibility="visible";
         document.getElementById('inputmenu').classList.add('fadeoutanim'); 
-        document.getElementById('resulttable').classList.add('fadeinanimtable');
+        document.getElementById('loader').style.visibility="visible";
+        
+        //end of valid checking, function is executed hereafter 
+        askGPT(information.a, information.b, information.c, information.d, information.e);
     }
+
 })
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+const API_KEY = process.env.API_KEY;  //REPLACE THIS WITH YOUR OWN API_KEY VALUE IN THE FILE ".env"!
+
+app.use(bodyParser.json());
+
+app.post('/getGPT3Response', async (req, res) => {
+    const promptText = req.body.prompt;
+
+    const headers = {
+        'Authorization': `Bearer ${'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'}`,
+        'Content-Type': 'application/json',
+        'User-Agent': 'OpenAI-GPT-Node.js'
+    };
+
+    const data = {
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "user", "content": promptText }
+        ]
+    };
+
+    try {
+        const response = await axios.post(OPENAI_API_URL, data, { headers });
+        res.json({ message: response.data.choices[0].message.content });
+    } catch (error) {
+        console.error('Error calling OpenAI:', error.response ? error.response.data : error.message);
+        res.status(500).json({ error: 'Failed to get response from GPT-3' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
